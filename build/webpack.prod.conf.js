e69045201c6ac411504f5a167f397d1c17265b69
dokechin <ttatsumi@ra2.so-net.ne.jp>
2018-05-14 07:28:06 +0900

    update cache setting

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -108,7 +108,17 @@ const webpackConfig = merge(baseWebpackConfig, {
       filename: 'service-worker.js',
       staticFileGlobs: ['dist/**/*.{js,html,css}'],
       minify: true,
-      stripPrefix: 'dist/'
+      stripPrefix: 'dist/',
+      runtimeCaching: [
+        {
+          urlPattern: /\.(jpg|png|svg|woff|ttf|eot)/,
+          handler: 'cacheFirst'
+        },
+        {
+          urlPattern: /^https:\/\/fonts\.googleapis\.com\//,
+          handler: 'cacheFirst'
+        }
+      ]
     })
   ]
 })
